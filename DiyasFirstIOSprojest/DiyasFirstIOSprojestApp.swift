a898edea181d316c9b8c5a205b75f560dc7368e4
diyashah <diyashah@udel.edu>
2024-02-19 17:00:08 -0500

    Initial Commit

---
+++ DiyasFirstIOSprojest/DiyasFirstIOSprojestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiyasFirstIOSprojestApp.swift
+//  DiyasFirstIOSprojest
+//
+//  Created by Diya Shah on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct DiyasFirstIOSprojestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
